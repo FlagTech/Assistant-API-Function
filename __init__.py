07aebb7cddb7b80121f32287283eca498d65328a
gamer00801 <91865766+gamer00801@users.noreply.github.com>
2023-12-20 18:01:07 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -114,7 +114,7 @@ def chat_with_functions(user_input, tools_table, thread_id,  assistant_id):
     while True:
         run = wait_on_run(run)
         if run.status == 'requires_action':
-            print('搜尋中...')
+            print('生成中...')
             tool_calls = run.required_action.submit_tool_outputs.tool_calls
             outputs = call_tools(tool_calls, tools_table)
             run = _client.beta.threads.runs.submit_tool_outputs(
